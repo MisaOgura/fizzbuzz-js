7556b44b23db4f540261ac6bc50c4fc7917decb8
Misa Ogura <misa.ogura01@gmail.com>
2016-03-29 13:30:34 +0100

    Finish FizzBuzz

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -3,13 +3,39 @@ describe("FizzBuzz", function() {
   var fizzBuzz;
 
   beforeEach(function() {
-    var fizzBuzz = new FizzBuzz();
+    fizzBuzz = new FizzBuzz();
   });
 
   describe("multipuls of 3", function() {
   it("Should return fizz when number is 3", function() {
     expect(fizzBuzz.play(3)).toEqual("fizz");
     });
+  it("Should return fizz when number is 6", function() {
+    expect(fizzBuzz.play(6)).toEqual("fizz");
+    });
   });
 
+  describe("multuples of 5", function() {
+    it("should return buzz when number is 5", function(){
+      expect(fizzBuzz.play(5)).toEqual('buzz');
+    });
+    it("should return buzz when number is 10", function(){
+      expect(fizzBuzz.play(10)).toEqual('buzz');
+    });
+  });
+
+  describe("multiples of 3 and 5", function(){
+    it("should return fizzbuzz when number is 15", function(){
+      expect(fizzBuzz.play(15)).toEqual('fizzbuzz');
+    });
+  });
+
+  describe("not divisible by 3 or 5", function(){
+    it("should return number when number is 7", function(){
+      expect(fizzBuzz.play(7)).toEqual(7);
+    });
+    it("should return number when number is 79", function(){
+      expect(fizzBuzz.play(79)).toEqual(79);
+    });
+  });
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -2,5 +2,22 @@ function FizzBuzz() {
 }
 
   FizzBuzz.prototype.play = function(number) {
-    return 'fizz';
+    if((number % 15 === 0)){
+      return 'fizzbuzz';
+    }
+    else if(number % 5 === 0){
+      return 'buzz';
+    }
+    else if(number % 3 === 0){
+      return 'fizz';
+    }
+    else {
+      return number
+    };
   };
+
+var fizzBuzz = new FizzBuzz();
+
+for(i=1; i<101; i += 1){
+  console.log(fizzBuzz.play(i));
+};
